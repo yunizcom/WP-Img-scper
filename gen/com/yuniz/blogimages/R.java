54661fb07535eec458651ea47d76d4e434a326e0
Stanly Lee <stanly.lns@gmail.com>
2013-11-01 17:04:44 +0800

    done ad inserted

--- gen/com/yuniz/blogimages/R.java
+++ gen/com/yuniz/blogimages/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
+        public static final int action_settings=0x7f080010;
+        public static final int adView=0x7f08000f;
         public static final int button1=0x7f080006;
         public static final int button1a=0x7f080007;
         public static final int button2=0x7f08000a;
